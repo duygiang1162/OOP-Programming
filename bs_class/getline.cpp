bae5152677e95a3847de8dd9e764e0097c5e23ff
giang <giang@little.lan>
2023-02-19 16:58:38 +0700

    To push on Ropo

--- bs_class/getline.cpp
+++ bs_class/getline.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std::string input;
-    std::cout << "Enter a line of text: ";
-    std::getline(std::cin, input);
-    std::cout << "You entered: " << input << std::endl;
-    return 0;
-}
+using namespace std;
+
+// int main() {
+//     std::string input;
+//     std::cout << "Enter a line of text: ";
+//     std::getline(std::cin, input);
+//     std::cout << "You entered: " << input << std::endl;
+//     return 0;
+// }
+
