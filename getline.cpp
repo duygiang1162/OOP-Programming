46cdc8d76b93d078fa197ab27279a53bbfaeb5ae
giang <giang@little.lan>
2023-02-19 15:30:20 +0700

    Additonal Knowledge about Gets and getline

---
+++ getline.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string input;
+    std::cout << "Enter a line of text: ";
+    std::getline(std::cin, input);
+    std::cout << "You entered: " << input << std::endl;
+    return 0;
+}
